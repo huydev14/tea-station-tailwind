8a29e815adb42499eedc4244990f636dec4e2005
trgiahuy14 <trgiahuy14@gmail.com>
2026-03-08 20:34:31 +0700

    feat(products): display all products from data file

--- js/main.js
+++ js/main.js
@@ -1,62 +1,62 @@
-import { partnerLogos, productList, partnerLogoBasePath } from "./data.js";
+import { partnerLogos, productList, partnerLogoBasePath } from './data.js';
 
 /* =================
  * Nav
  ====================*/
 $(function () {
-	$(".navbar").hidescroll();
+    $('.navbar').hidescroll();
 
-	// Mobile dropdown
-	const toggleBtn = $("#toggle_btn");
-	const dropdownMenu = $(".dropdown-menu");
+    // Mobile dropdown
+    const toggleBtn = $('#toggle_btn');
+    const dropdownMenu = $('.dropdown-menu');
 
-	toggleBtn.click(() => {
-		dropdownMenu.toggleClass("open");
-	});
+    toggleBtn.click(() => {
+        dropdownMenu.toggleClass('open');
+    });
 });
 
 /* =================
  * PARTNER LOGOS
  ====================*/
 $(function () {
-	const container = document.getElementById("partner-logo-list");
+    const container = document.getElementById('partner-logo-list');
 
-	partnerLogos.forEach((logo) => {
-		const img = document.createElement("img");
+    partnerLogos.forEach((logo) => {
+        const img = document.createElement('img');
 
-		img.src = partnerLogoBasePath + logo.fileName;
-		img.alt = logo.alt;
-		img.classList.add("logo-ticker-image");
-		container.appendChild(img);
-	});
+        img.src = partnerLogoBasePath + logo.fileName;
+        img.alt = logo.alt;
+        img.classList.add('logo-ticker-image');
+        container.appendChild(img);
+    });
 });
 
 /* =================
  * PRODUCTS
  ====================*/
 $(function () {
-	// Active tab
-	$("li:first").addClass("activeTab");
+    // Active tab
+    $('li:first').addClass('activeTab');
 
-	// Change color active tab
-	$("li").on("click", function () {
-		$("li").removeClass("activeTab");
-		$('div[id="products-tabs"] ul .r-tabs-state-active').addClass("activeTab");
-	});
+    // Change color active tab
+    $('li').on('click', function () {
+        $('li').removeClass('activeTab');
+        $('div[id="products-tabs"] ul .r-tabs-state-active').addClass('activeTab');
+    });
 
-	$("#products-tabs").responsiveTabs({
-		animation: "slide",
-	});
+    $('#products-tabs').responsiveTabs({
+        animation: 'slide',
+    });
 });
 
 /* =================
  * Best Sellers
  ====================*/
 $(function () {
-	$(".slider").slick({
-		autoplay: true,
-		dots: true,
-	});
+    $('.slider').slick({
+        autoplay: true,
+        dots: true,
+    });
 });
 
 /* =================
@@ -64,22 +64,46 @@ $(function () {
  ====================*/
 
 $(function () {
-	const counterUp = window.counterUp.default;
+    const counterUp = window.counterUp.default;
 
-	const callback = (entries) => {
-		entries.forEach((entry) => {
-			const el = entry.target;
-			if (entry.isIntersecting && !el.classList.contains("is-visible")) {
-				counterUp(el, {
-					duration: 2000,
-					delay: 16,
-				});
-				el.classList.add("is-visible");
-			}
-		});
-	};
+    const callback = (entries) => {
+        entries.forEach((entry) => {
+            const el = entry.target;
+            if (entry.isIntersecting && !el.classList.contains('is-visible')) {
+                counterUp(el, {
+                    duration: 2000,
+                    delay: 16,
+                });
+                el.classList.add('is-visible');
+            }
+        });
+    };
 
-	const IO = new IntersectionObserver(callback, { threshold: 1 });
+    const IO = new IntersectionObserver(callback, { threshold: 1 });
 
-	const el = document.querySelectorAll(".counter").forEach((node) => IO.observe(node));
+    const el = document.querySelectorAll('.counter').forEach((node) => IO.observe(node));
+});
+
+/* =================
+ * All products
+ ====================*/
+$(function () {
+    productList.map((product) => {
+        $('#product-items--container').append(`
+			<div class="group relative col-span-3 overflow-hidden hover:shadow-md">
+				<div class="portfolio-item">
+					<div>
+						<img src="${product.img}" alt="product-img" />
+
+						<div class="product-item-overlay">
+							<div class="product-details">
+								<h3>${product.name}</h3>
+								<p>${product.description}</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		`);
+    });
 });
